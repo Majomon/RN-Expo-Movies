5e434c79afbacc4e222cb0841fb4447d2d0fafc3
Mauri <mauri.monzon91@gmail.com>
2024-11-19 16:25:39 -0300

    Mapeando peliculas

--- core/actions/movies/now-playing.action.ts
+++ core/actions/movies/now-playing.action.ts
@@ -1,10 +1,17 @@
 import { MovieDBMovieResponse } from "@/infrastructure/interfaces/moviedb-response";
 import { movieApi } from "../api/movie-api";
+import { MovieMapper } from "@/infrastructure/mappers/movie.mapper";
 
 export const nowPlayinAction = async () => {
   try {
     const { data } = await movieApi.get<MovieDBMovieResponse>("/now_playing");
-    return [];
+    // console.log(JSON.stringify(data, null, 2));
+
+    // Es lo mismo que el que esta abajo . Si tengo un argumento que paso a otra funcion puedo obviarlo
+    // const movies = data.results.map((movie)=>MovieMapper.fromTheMovieDBToMovie(movie));
+    const movies = data.results.map(MovieMapper.fromTheMovieDBToMovie);
+
+    return movies;
   } catch (error) {
     console.log(error);
     throw "No se pudo obtener las películas";
