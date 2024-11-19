0bfb13025f99d49af9ccfb529ae8ab3dd7c046ed
Mauri <mauri.monzon91@gmail.com>
2024-11-19 16:44:34 -0300

    Pantalla de carga

--- app/home/index.tsx
+++ app/home/index.tsx
@@ -1,15 +1,25 @@
-import { View, Text } from "react-native";
+import { View, Text, ActivityIndicator } from "react-native";
 import React from "react";
 import { useMovies } from "@/presentation/hooks/useMovies";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 const HomeScreen = () => {
+  const safeArea = useSafeAreaInsets();
   const { nowPlayingQuery } = useMovies();
 
-  return (
-    <View>
-      <Text>HomeScreen</Text>
+  if (nowPlayingQuery.isLoading) {
+    return (
+      <View className="flex-1 justify-center">
+        <ActivityIndicator color="purple" size={50} />
+      </View>
+    );
+  }
 
-      <Text>{JSON.stringify(nowPlayingQuery, null, 2)}</Text>
+  return (
+    <View className="mt-2" style={{ paddingTop: safeArea.top }}>
+      <Text className="text-3xl font-bolt px-4 mb-2">
+        {JSON.stringify(nowPlayingQuery, null, 2)}
+      </Text>
     </View>
   );
 };
